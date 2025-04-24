9c6a09ecbf8304c782f2d5eebaf4775ee69e7357
RavnOP <abhishek82006@gmail.com>
2025-04-24 16:37:34 +0530

    Initial commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "app.movienations.me"
+  const baseUrl = "https://app.movienations.me"
 
   return [
     {
